c58ba673e284837d608bcd0a7c9446213db69169
vrishabh Kashyap <vrishabh2468@gmail.com>
2026-03-29 15:32:35 +0530

    added validation

--- components/ExpenseForm.tsx
+++ components/ExpenseForm.tsx
@@ -5,7 +5,8 @@ import { useRouter } from "next/navigation"
 
 const ExpenseForm = () => {
     const router = useRouter()
-    const [loading , setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
 
     const [name, setName] = useState("")
     const [amount, setAmount] = useState("")
@@ -13,8 +14,16 @@ const ExpenseForm = () => {
 
     const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault();
+
+        if (!name) {
+            setError("Name is required")
+            return
+        }
+        if (!amount) {
+            setError("amount is required")
+            return
+        }
         setLoading(true)
-        
 
         try {
             const res = await fetch("/api/expenses", {
@@ -41,21 +50,27 @@ const ExpenseForm = () => {
 
     return (
         <div className="">
-            <form className="border shadow-lg shadow-gray-500/50 border-gray-200 rounded-lg p-8 flex flex-col space-y-3 justify-center max-w-200 mx-auto m-10" onSubmit={handleSubmit} action="">
+            <form className="border relative shadow-lg shadow-gray-500/50 border-gray-200 rounded-lg p-8 flex flex-col space-y-3 justify-center max-w-200 mx-auto m-10" onSubmit={handleSubmit} action="">
                 <h2 className="text-lg font-semibold">Expense Name</h2>
-                <input className="border outline-none border-gray-200 px-4 py-2 rounded-lg" value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Expense name" />
+                <input className="border outline-none border-gray-200 px-4 py-2 rounded-lg" value={name} onChange={(e) => {setName(e.target.value); setError("")}} type="text" placeholder="Enter Expense name" />
                 <h2 className="text-lg font-semibold">Amount</h2>
-                <input className="border outline-none border-gray-200 px-4 py-2 rounded-lg" value={amount} onChange={(e) => setAmount(e.target.value)} type="number" placeholder="Enter Amount" />
+                <input className="border outline-none border-gray-200 px-4 py-2 rounded-lg" value={amount} onChange={(e) =>{ setAmount(e.target.value); setError("")}} type="number" placeholder="Enter Amount" />
                 <h2 className="text-lg font-semibold">Category</h2>
-                <select value={category} onChange={(e)=>setCategory(e.target.value)} className="text-white bg-[#0a0a0a] p-2 border rounded-lg" name="category" id="category" required>
+                <select value={category} onChange={(e) => setCategory(e.target.value)} className="text-white bg-[#0a0a0a] p-2 border rounded-lg" name="category" id="category" required>
                     <option value="food">Food</option>
                     <option value="hardware">Hardware</option>
                     <option value="footwear">footwear</option>
                     <option value="clothes">Clothes</option>
                 </select>
 
-                <button disabled={loading} className="border px-4 py-2 mt-4 font-bold bg-gray-300 text-black shadow-lg shadow-gray-500/50 hover:bg-gray-400 rounded-xl" type="submit">{loading ? "Adding...": "Add Expense"}</button>
+                <button disabled={loading} className="border px-4 py-2 mt-4 font-bold bg-gray-300 text-black shadow-lg shadow-gray-500/50 hover:bg-gray-400 rounded-xl" type="submit">{loading ? "Adding..." : "Add Expense"}</button>
+
+                {error && <div className={` bg-red-200 z-10 transition-all duration-100 absolute top-1 font-semibold right-2 px-4 py-2 rounded-lg `}>
+                    <div className="text-red-500">{error}</div>
+                </div>}
             </form>
+
+
 
         </div>
     )
